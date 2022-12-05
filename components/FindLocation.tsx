08bae71a6a3c57dec44657cffda26a07d3bde476
MustaphaELMEZZIOUY <mustaphaelmezziouy1997@gmail.com>
2022-12-05 23:15:23 +0100

    adding google maps api

--- components/FindLocation.tsx
+++ components/FindLocation.tsx
@@ -1,8 +1,28 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Title } from './Title'
+import { Loader } from '@googlemaps/js-api-loader';
 
 export const FindLocation = () => {
+    const googlemap = useRef(null) as any;
+
+    useEffect(() => {
+        // mayn "AIzaSyCqgdYWyVh3YPu_tXp5afoyEm5IKktGQao"
+        // AIzaSyDRX0D21tjCpNmpABQp8bnfNyA99pscQrM
+        const loader = new Loader({
+            apiKey: process.env.VITE_GOOGLE_MAPS_API_KEY as string,
+            version: 'weekly',
+        });
+        let map;
+        loader.load().then(() => {
+            map = new google.maps.Map(googlemap.current, {
+                
+                center: { lat: 33.97120698238111, lng: -6.862241453277229 },
+                zoom: 8,
+            });
+        });
+    });
+
     return (
         <div className="w-full my-8 px-4 md:px-8 md:my-12 md:grid md:grid-cols-3 md:gap-2">
             <div className="flex flex-col items-center rounded-md bg-slate-200">
@@ -22,7 +42,7 @@ export const FindLocation = () => {
             </div>
 
             <div className="col-span-2 md:order-first rounded-md bg-slate-200">
-                
+                <div id="map" ref={googlemap}/>
             </div>
         </div>
     )
